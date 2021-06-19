754e804a81135f3fb9ff561537214063caeb9cbe
Mahmoud Bassiony <mahbassiony@banquemisr.com>
2021-06-19 15:58:57 +0200

    Make build targets for dev/test/ua/production

--- Metaweather/Metaweather/Resource/SupportingFile/Config.swift
+++ Metaweather/Metaweather/Resource/SupportingFile/Config.swift
@@ -9,11 +9,18 @@
 import Foundation
 
 public struct Config {
-   
+    private var properties: [String: String] = [:]
     public var hostURL: String {
-        return "https://www.metaweather.com"
+        return "https://" + (properties["HOST_URL"] ?? "")
     }
+    
     public var baseURL: String {
-        return "https://www.metaweather.com/api/"
+        return "https://" + (properties["BASE_URL"] ?? "")
+    }
+    
+    public init() {
+        for (key, value) in (Bundle.main.infoDictionary) ?? [:] {
+            properties[key] = value as? String ?? ""
+        }
     }
 }
